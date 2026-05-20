5366da95d0993d1e10dfc1eb3a1d87ce5463ad7f
anjali <as0172085@gmail.com>
2026-05-20 12:31:05 +0530

    control flow complete

--- 04-control-flow/switch.js
+++ 04-control-flow/switch.js
@@ -1,15 +1,45 @@
-let month = 2;
-switch (month) {
-    case 1:
-        console.log("January");
-        break;
-    case 2:
-        console.log("February");
-        break;
-    case 3:
-        console.log("March");
-        break;
-    default:
-        console.log("Invalid month");
-        break;
-}
+// let month = 2;
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("Invalid month");
+//         break;
+// }
+
+
+// truthy values and falsy values
+// let email = "anjalishrivastava@gmail.com"
+// if (email) {
+//     console.log("User email exist")
+// }
+// else{
+//     console.log("email don't exist")
+// }
+
+// falsy values - "", false , 0, nan, On, null, undefined, -0, BigInt
+// truthy -  true, " ", "xyz", [], many more
+
+// Nullish collascing operator (??): null undefined
+let val1; 
+// val1 = 5 ?? 10
+// val1 = null ?? 10;
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 20
+// console.log(val1)
+
+
+// Ternary operator
+
+// condition ? true : false
+
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("ice tea is more than 80") : console.log("ice tea is less than 80")
